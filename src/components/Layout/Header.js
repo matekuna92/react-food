481e2d7763320966325fc77e276d186b7abfe70a
matekuna92 <kunamate66@gmail.com>
2022-12-27 18:07:42 +0100

    food app - Header with styles

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+
+import mealsImg from '../../assets/meals.jpg';
+import styles from './Header.module.css';
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={styles.header}>
+                <h1> ReactMeals </h1>
+                <button>Cart</button>
+            </header>
+            <div className={styles['main-image']}>
+                <img src={mealsImg} alt='A table full of delicious food' />
+            </div>
+        </Fragment>
+    )
+};
+
+export default Header;
