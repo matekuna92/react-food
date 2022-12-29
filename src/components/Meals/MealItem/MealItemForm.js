063dc74927dcb0b1dde812fcf5d7c0aebe72842d
matekuna92 <kunamate66@gmail.com>
2022-12-29 21:31:23 +0100

    useref, access Input entered value from MealItemForm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,30 @@
+import { useRef } from 'react';
+
 import styles from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
+    const amountInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        // get access to Input through ref, so entere value can be accessed here
+        const enteredAmount = amountInputRef.current.value;         // always current, when using ref! value: value of the input,
+        const enteredAmountNumber = +enteredAmount;                 // because ref refers to an Input html element
+
+        if(enteredAmount.trim().length === 0 || enteredAmount < 1 || enteredAmount > 5) {
+            return;
+        }
+
+        
+    };
+
     return (
-        <form className={styles.form}>
-            <Input label='Amount' input={{
+        <form className={styles.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label='Amount' input={{
                 id: 'amount_' + props.id,
                 type: 'number',
                 min: '1',
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,14 +1,16 @@
+import React from 'react';
+
 import styles from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
     // {...props.input} : pass multiple attributes from outside. All key-value pairs are added as props to the input
     // e.g: id='1', type='text', 'name='input' -> all of these are added as props, so it can be configurable dynamically
     return (
         <div className={styles.input}>
             <label htmlFor={props.input.id}> {props.label} </label>
-            <input {...props.input}></input>
+            <input ref={ref} {...props.input}></input>
         </div>
-    );
-};
+        );
+});
 
 export default Input;
