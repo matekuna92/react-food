95b1e8e31e8fbdce57bec17f7db59cfc01ca2977
matekuna92 <kunamate66@gmail.com>
2023-01-04 12:53:40 +0100

    add bump class when item added to cart - useeffect, usestate

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,36 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
     const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => { return currentNum + item.amount; }, 0);
+    const buttonClasses = `${styles.button} ${buttonIsHighlighted ? styles.bump : ''}`;
+
+    // pull out items, set in the useEffect would run every time when something changes in the cartCtx, if we pass the whole cartCtx object as dependency
+    const { items } = cartCtx;  
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+
+        setButtonIsHighlighted(true);
+        // remove bump class, so bump effect doesnt run only when class is added for the first time
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
 
     return (
-            <button className={styles.button} onClick={props.onClick}>
+            <button className={buttonClasses} onClick={props.onClick}>
                 <span className={styles.icon}>
                 <CartIcon />
             </span>
