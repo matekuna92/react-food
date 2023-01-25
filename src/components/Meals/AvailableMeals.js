6780e16cad69fda69db725086f7e162a94da38fe
matekuna92 <kunamate66@gmail.com>
2023-01-25 21:36:04 +0100

    meals fetched from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,42 @@
+import { useState, useEffect } from 'react';
+
 import styles from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-    ];
-
 const AvailableMeals = () => {
+    const [meals, setMeals] = useState([]);
+
+    // function passed  to useEffect should not return promise, it should return the cleanup function
+    // thats why the nested fetchMeals function is needed
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-http-9c568-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            const responseData  = await response.json();
+            //  fetch('https://react-http-9c568-default-rtdb.europe-west1.firebasedatabase.app/meals.json')
+            //  .then(response => response.json())
+            //  .then(data => console.log(data));
+
+            const fetchedMeals = [];
+
+            for(const item in responseData) {
+                fetchedMeals.push({
+                    id: item,
+                    name: responseData[item].name,
+                    description: responseData[item].description,
+                    price: responseData[item].price
+                });
+            }
+
+            setMeals(fetchedMeals);
+            console.log(meals);
+        }
+
+        fetchMeals();
+    }, []);
+
     // transform array to array of JSX elements
-    const mealsList = DUMMY_MEALS.map(meal =>
+    const mealsList = meals.map(meal =>
         <MealItem
             id={meal.id}
             key={meal.id}
